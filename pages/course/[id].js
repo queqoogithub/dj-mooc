78836c9fe57fd7bf39d8f8ef805ae72d349f41f0
queqoomts <narathip@mtsgoldgroup.com>
2022-04-08 17:39:35 +0700

    react-player
    add vdo player ไปที่ page course id

--- pages/course/[id].js
+++ pages/course/[id].js
@@ -1,14 +1,21 @@
 import React, { useContext, useEffect, useReducer, useState } from 'react';
 import { useRouter } from 'next/router'
+import ReactPlayer from 'react-player'
 
 export default function Course() {
   const router = useRouter()
-  const {id} = router.query
+  const { id } = router.query
   console.log('Params Group', id)
 
 
-  return(
-    <> Course ID: { id } </>
+  return (
+    <>
+      Course ID: {id}
+      <div>
+        <h2>NextJs VideoPlayer - GeeksforGeeks</h2>
+        <ReactPlayer url='https://www.youtube.com/watch?v=wWgIAphfn2U' />
+      </div>
+    </>
   );
 
 }
